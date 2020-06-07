429345d70c1e226de8dbbb6d9bdf3dafe9a179cc
nhatminh2947 <nhatminh2947@gmail.com>
2020-06-07 21:44:27 +0800

    change network

--- models/third_model.py
+++ models/third_model.py
@@ -1,4 +1,5 @@
 import numpy as np
+from ray.rllib.models.torch.misc import SlimConv2d
 from ray.rllib.models.torch.torch_modelv2 import TorchModelV2
 from ray.rllib.utils import try_import_torch
 
@@ -10,66 +11,73 @@ class ActorCriticModel(nn.Module, TorchModelV2):
         TorchModelV2.__init__(self, obs_space, action_space, num_outputs, model_config, name)
         nn.Module.__init__(self)
 
-        self.shared_layers = nn.Sequential(
-            nn.Conv2d(
-                in_channels=in_channels,
-                out_channels=32,
-                kernel_size=3,
+        layers = []
+        prev_layer_channels = in_channels
+        num_channels_per_layer = 64
+        for i in range(8):
+            layers.append(SlimConv2d(
+                in_channels=prev_layer_channels,
+                out_channels=num_channels_per_layer,
+                kernel=3,
+                stride=1,
                 padding=1,
-                stride=1),
-            nn.ReLU(),
-            nn.Conv2d(
-                in_channels=32,
-                out_channels=64,
-                kernel_size=3,
-                padding=1,
-                stride=1),
-            nn.ReLU(),
-            nn.Conv2d(
-                in_channels=64,
-                out_channels=128,
-                kernel_size=3,
-                stride=1),
-            nn.ReLU(),
-            Flatten(),
-            nn.Linear(9 * 9 * 128, 256),
-            nn.ReLU(),
-            nn.Linear(256, feature_dim),
-            nn.ReLU()
-        )
+            ))
+
+            prev_layer_channels = num_channels_per_layer
+
+        self.shared_layers = nn.Sequential(*layers)
+        # self.shared_layers = nn.Sequential(
+        #     nn.Conv2d(
+        #         in_channels=in_channels,
+        #         out_channels=32,
+        #         kernel_size=3,
+        #         padding=1,
+        #         stride=1),
+        #     nn.ReLU(),
+        #     nn.Conv2d(
+        #         in_channels=32,
+        #         out_channels=64,
+        #         kernel_size=3,
+        #         padding=1,
+        #         stride=1),
+        #     nn.ReLU(),
+        #     nn.Conv2d(
+        #         in_channels=64,
+        #         out_channels=128,
+        #         kernel_size=3,
+        #         stride=1),
+        #     nn.ReLU(),
+        #     Flatten(),
+        #     nn.Linear(9 * 9 * 128, 256),
+        #     nn.ReLU(),
+        #     nn.Linear(256, feature_dim),
+        #     nn.ReLU()
+        # )
 
         self.actor_layers = nn.Sequential(
-            nn.Linear(feature_dim, 256),
+            nn.Conv2d(
+                in_channels=prev_layer_channels,
+                out_channels=2,
+                kernel_size=1,
+                padding=0,
+                stride=1),
             nn.ReLU(),
-            nn.Linear(256, action_space.n)
+            nn.Linear(2 * 11 * 11, action_space.n)
         )
 
         self.critic_layers = nn.Sequential(
-            nn.Linear(feature_dim, 512),
+            nn.Conv2d(
+                in_channels=prev_layer_channels,
+                out_channels=1,
+                kernel_size=1,
+                padding=0,
+                stride=1),
             nn.ReLU(),
-            nn.Linear(in_features=512, out_features=1)
+            nn.Linear(11 * 11, 32),
+            nn.Linear(32, 1),
         )
 
         self._shared_layer_out = None
-
-        for p in self.modules():
-            if isinstance(p, nn.Conv2d):
-                nn.init.orthogonal_(p.weight, np.sqrt(2))
-                p.bias.data.zero_()
-
-            if isinstance(p, nn.Linear):
-                nn.init.orthogonal_(p.weight, np.sqrt(2))
-                p.bias.data.zero_()
-
-        for i in range(len(self.actor_layers)):
-            if type(self.actor_layers[i]) == nn.Linear:
-                nn.init.orthogonal_(self.actor_layers[i].weight, 0.01)
-                self.actor_layers[i].bias.data.zero_()
-
-        for i in range(len(self.critic_layers)):
-            if type(self.critic_layers[i]) == nn.Linear:
-                nn.init.orthogonal_(self.critic_layers[i].weight, 0.1)
-                self.critic_layers[i].bias.data.zero_()
 
     def forward(self, input_dict, state, seq_lens):
         x = input_dict["obs"]
