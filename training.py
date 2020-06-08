617d17c25384c75706ebf8740412ea599e7fd813
nhatminh2947 <nhatminh2947@gmail.com>
2020-06-08 16:41:29 +0800

    change perturbation interval

--- training.py
+++ training.py
@@ -64,8 +64,8 @@ def training_team(params):
 
     env_config = {
         "env_id": env_id,
-        "render": params['render'],
-        "game_state_file": params['game_state_file']
+        "render": params["render"],
+        "game_state_file": params["game_state_file"]
     }
 
     env = pommerman.make(env_id, [])
@@ -103,12 +103,12 @@ def training_team(params):
         return "static"
 
     pbt_scheduler = PopulationBasedTraining(
-        time_attr='training_iteration',
-        metric='episode_reward_mean',
-        mode='max',
-        perturbation_interval=50,
+        time_attr="training_iteration",
+        metric="policy_reward_mean/policy_0",
+        mode="max",
+        perturbation_interval=10,
         hyperparam_mutations={
-            "lr": lambda: random.uniform(0.00001, 0.05),
+            # "lr": lambda: random.uniform(0.00001, 0.05),
             "gamma": lambda: random.uniform(0.5, 0.999)
         })
 
@@ -119,33 +119,33 @@ def training_team(params):
         name=params["name"],
         queue_trials=True,
         scheduler=pbt_scheduler,
-        num_samples=params['num_samples'],
+        num_samples=params["num_samples"],
         stop={
             # "training_iteration": params["training_iteration"],
-            "timesteps_total": 100000000
+            "timesteps_total": 1000000000
         },
         checkpoint_freq=params["checkpoint_freq"],
         checkpoint_at_end=True,
         config={
-            "gamma": params['gamma'],
+            "gamma": params["gamma"],
             "lr": params["lr"],
             "entropy_coeff": params["entropy_coeff"],
             "kl_coeff": 0.0,  # disable KL
-            "batch_mode": "complete_episodes" if params['complete_episodes'] else "truncate_episodes",
-            "rollout_fragment_length": params['rollout_fragment_length'],
+            "batch_mode": "complete_episodes" if params["complete_episodes"] else "truncate_episodes",
+            "rollout_fragment_length": params["rollout_fragment_length"],
             "env": PommeMultiAgent,
             "env_config": env_config,
-            "num_workers": params['num_workers'],
-            "num_envs_per_worker": params['num_envs_per_worker'],
-            "num_gpus_per_worker": params['num_gpus_per_worker'],
-            "num_gpus": params['num_gpus'],
-            "train_batch_size": params['train_batch_size'],
-            "sgd_minibatch_size": params['sgd_minibatch_size'],
-            "clip_param": params['clip_param'],
-            "lambda": params['lambda'],
-            "num_sgd_iter": params['num_sgd_iter'],
+            "num_workers": params["num_workers"],
+            "num_envs_per_worker": params["num_envs_per_worker"],
+            "num_gpus_per_worker": params["num_gpus_per_worker"],
+            "num_gpus": params["num_gpus"],
+            "train_batch_size": params["train_batch_size"],
+            "sgd_minibatch_size": params["sgd_minibatch_size"],
+            "clip_param": params["clip_param"],
+            "lambda": params["lambda"],
+            "num_sgd_iter": params["num_sgd_iter"],
             "vf_share_layers": True,
-            "vf_loss_coeff": params['vf_loss_coeff'],
+            "vf_loss_coeff": params["vf_loss_coeff"],
             "vf_clip_param": params["vf_clip_param"],
             "callbacks": PommeCallbacks,
             "multiagent": {
@@ -163,35 +163,35 @@ def training_team(params):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--num_workers', type=int, default=0, help='number of worker')
-    parser.add_argument('--num_gpus', type=float, default=1, help='number of gpu')
-    parser.add_argument('--train_batch_size', type=int, default=8192)
-    parser.add_argument('--sgd_minibatch_size', type=int, default=128)
-    parser.add_argument('--clip_param', type=float, default=0.2)
-    parser.add_argument('--lambda', type=float, default=0.95)
-    parser.add_argument('--gamma', type=float, default=0.999)
-    parser.add_argument('--num_sgd_iter', type=int, default=3)
-    parser.add_argument('--vf_loss_coeff', type=float, default=0.5)
-    parser.add_argument('--lr', type=float, default=1e-4)
-    parser.add_argument('--entropy_coeff', type=float, default=0.001)
-    parser.add_argument('--complete_episodes', action='store_true')
-    parser.add_argument('--rollout_fragment_length', type=int, default=128)
-    parser.add_argument('--training_iteration', type=int, default=1000)
-    parser.add_argument('--checkpoint_freq', type=int, default=100)
-    parser.add_argument('--num_envs_per_worker', type=int, default=1)
-    parser.add_argument('--num_gpus_per_worker', type=float, default=0.0)
-    parser.add_argument('--vf_clip_param', type=float, default=2.0)
-    parser.add_argument('--name', type=str, default="experiment")
-    parser.add_argument('--game_state_file', type=str, default=None)
-    parser.add_argument('--render', action='store_true')
-    parser.add_argument('--local_mode', action='store_true')
-    parser.add_argument('--resume', action='store_true')
-    parser.add_argument('--restore', type=str, default=None)
-    parser.add_argument('--num_samples', type=int, default=4)
+    parser.add_argument("--num_workers", type=int, default=0, help="number of worker")
+    parser.add_argument("--num_gpus", type=float, default=1, help="number of gpu")
+    parser.add_argument("--train_batch_size", type=int, default=8192)
+    parser.add_argument("--sgd_minibatch_size", type=int, default=128)
+    parser.add_argument("--clip_param", type=float, default=0.2)
+    parser.add_argument("--lambda", type=float, default=0.95)
+    parser.add_argument("--gamma", type=float, default=0.999)
+    parser.add_argument("--num_sgd_iter", type=int, default=3)
+    parser.add_argument("--vf_loss_coeff", type=float, default=0.5)
+    parser.add_argument("--lr", type=float, default=1e-4)
+    parser.add_argument("--entropy_coeff", type=float, default=0.001)
+    parser.add_argument("--complete_episodes", action="store_true")
+    parser.add_argument("--rollout_fragment_length", type=int, default=128)
+    parser.add_argument("--training_iteration", type=int, default=1000)
+    parser.add_argument("--checkpoint_freq", type=int, default=10)
+    parser.add_argument("--num_envs_per_worker", type=int, default=1)
+    parser.add_argument("--num_gpus_per_worker", type=float, default=0.0)
+    parser.add_argument("--vf_clip_param", type=float, default=2.0)
+    parser.add_argument("--name", type=str, default="experiment")
+    parser.add_argument("--game_state_file", type=str, default=None)
+    parser.add_argument("--render", action="store_true")
+    parser.add_argument("--local_mode", action="store_true")
+    parser.add_argument("--resume", action="store_true")
+    parser.add_argument("--restore", type=str, default=None)
+    parser.add_argument("--num_samples", type=int, default=4)
     args = parser.parse_args()
     params = vars(args)
 
     ray.shutdown()
-    ray.init(local_mode=params['local_mode'], object_store_memory=4e10)
+    ray.init(local_mode=params["local_mode"], object_store_memory=4e10)
 
     training_team(params)
