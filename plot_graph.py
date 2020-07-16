4a70f6ae64430c7b3858fad16ee06e7abc8a50a8
nhatminh2947 <nhatminh2947@gmail.com>
2020-07-16 21:22:50 +0800

    add plot graph

---
+++ plot_graph.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+exp_1 = pd.read_csv(
+    '/home/lucius/ray_results/2vs2_center_obs/PPO_PommeMultiAgent-v2_0_2020-07-16_02-46-00co9kkax2/progress.csv')
+exp_2 = pd.read_csv(
+    '/home/lucius/ray_results/2vs2_center_obs/PPO_PommeMultiAgent-v2_0_2020-07-15_19-44-100rjtw3d7/progress.csv')
+
+fig, ax = plt.subplots()
+# progress['custom_metrics/agent_training_0_0/EnemyDeath_mean'].plot()
+ax.plot(exp_1['timesteps_total'], exp_1['custom_metrics/agent_training_0_0/EnemyDeath_mean'],
+        label='Experiment 1 - Full board')
+ax.plot(exp_2['timesteps_total'], exp_2['custom_metrics/agent_training_0_0/EnemyDeath_mean'],
+        label='Experiment 2 - Center observation')
+ax.set_xlim(0, 22000000)
+
+ax.set_xlabel('timesteps')
+ax.set_ylabel('Enemy Death Mean')
+ax.set_title('Enemy Death')
+ax.legend()
+plt.show()
+
+fig, ax = plt.subplots()
+# progress['custom_metrics/agent_training_0_0/EnemyDeath_mean'].plot()
+ax.plot(exp_1['timesteps_total'], exp_1['custom_metrics/agent_training_0_0/RealBombs_mean'],
+        label='Experiment 1 - Full board - Agent 1')
+ax.plot(exp_1['timesteps_total'], exp_1['custom_metrics/agent_training_0_2/RealBombs_mean'],
+        label='Experiment 1 - Full board - Agent 2')
+ax.plot(exp_2['timesteps_total'], exp_2['custom_metrics/agent_training_0_0/RealBombs_mean'],
+        label='Experiment 2 - Center observation - Agent 1')
+ax.plot(exp_2['timesteps_total'], exp_2['custom_metrics/agent_training_0_2/RealBombs_mean'],
+        label='Experiment 2 - Center observation - Agent 2')
+ax.set_xlim(0, 22000000)
+
+ax.set_xlabel('timesteps')
+ax.set_ylabel('Average number of bomb placed')
+ax.set_title('Number of Bomb placed per episode')
+ax.legend()
+plt.show()
