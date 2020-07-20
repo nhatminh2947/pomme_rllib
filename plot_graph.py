4a5283bd0cf96d444b59ca3bb4455b0ae412a4d1
nhatminh2947 <nhatminh2947@gmail.com>
2020-07-21 00:21:41 +0800

    add entropy and vf loss plot

--- plot_graph.py
+++ plot_graph.py
@@ -37,3 +37,36 @@ ax.set_ylabel('Average number of bomb placed')
 ax.set_title('Number of Bomb placed per episode')
 ax.legend()
 plt.show()
+
+fig, ax = plt.subplots()
+# progress['custom_metrics/agent_training_0_0/EnemyDeath_mean'].plot()
+ax.plot(exp_1['timesteps_total'], exp_1['info/learner/policy_0/vf_loss'],
+        label='Experiment 1 - Full board')
+ax.plot(exp_2['timesteps_total'], exp_2['info/learner/policy_0/vf_loss'],
+        label='Experiment 2 - Center observation')
+ax.set_xlim(0, 22000000)
+
+ax.set_xlabel('timesteps')
+ax.set_ylabel('Loss')
+ax.set_title('Value Function Loss')
+ax.legend()
+plt.show()
+
+
+fig, ax = plt.subplots()
+# progress['custom_metrics/agent_training_0_0/EnemyDeath_mean'].plot()
+ax.plot(exp_1['timesteps_total'], exp_1['info/learner/policy_0/entropy'],
+        label='Experiment 1 - Full board')
+ax.plot(exp_2['timesteps_total'], exp_2['info/learner/policy_0/entropy'],
+        label='Experiment 2 - Center observation')
+ax.set_xlim(0, 22000000)
+
+ax.set_xlabel('timesteps')
+ax.set_ylabel('entropy')
+ax.set_title('Entropy')
+ax.legend()
+plt.show()
+
+print(exp_1.columns)
+print(exp_1['info/learner/policy_0/vf_loss'])
+print(exp_1['info/learner/policy_0/entropy'])
