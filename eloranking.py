e89e8c27bcbd0031b3602b9060cfcaa38f814d10
nhatminh2947 <nhatminh2947@gmail.com>
2020-08-30 19:28:10 +0800

    set alpha to 0

--- eloranking.py
+++ eloranking.py
@@ -49,11 +49,12 @@ class EloRatingSystem:
         self.population["policy_9"].rating = 1140
 
     def update_alpha(self, policy_name, enemy_death_mean):
-        self.population[policy_name].alpha = 1 - np.tanh(self.alpha_coeff * enemy_death_mean)
-        if self.population[policy_name].num_steps >= self.burn_in:
-            self.population[policy_name].alpha = 0
-            return 0.0
-        return self.population[policy_name].alpha
+        # self.population[policy_name].alpha = 1 - np.tanh(self.alpha_coeff * enemy_death_mean)
+        # if self.population[policy_name].num_steps >= self.burn_in:
+        #     self.population[policy_name].alpha = 0
+        #     return 0.0
+        # return self.population[policy_name].alpha
+        return 0
 
     def get_alpha(self, policy_name):
         return self.population[policy_name].alpha
